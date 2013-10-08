87b18402560c684e4b67688eef2c89f282bb154d
Kaniska <murugdev.eee@gmail.com>
2013-10-08 23:48:47 +0530

    New changes

---
+++ application/views/welcome/faq.php
@@ -0,0 +1,101 @@
+<?php $this->load->view('welcome/banner'); ?>
+
+</div> <!-- end of header_wrapper -->
+
+<div id="templatemo_content_wrapper_outer">
+<div id="templatemo_content_wrapper_inner">
+<div id="templatemo_content_wrapper">
+
+    <div id="templatemo_content_full">
+    
+    	<h1>Frequently Asked Question</h1>
+        
+        <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent aliquam velit a magna sodales quis elementum ipsum auctor. Quisque sem orci, malesuada eu  blandit et, bibendum facilisis mi. Vestibulum justo tortor, congue ut  cursus vel, fringilla a felis.</p>
+        
+   
+        <div class="services_section">
+        
+            <h4><a href="#">Q1] How to post a new Ride ?</a></h4>
+
+          <div class="services_content">
+            
+                <div class="left_full">
+                	<p>Mauris suscipit mauris in purus mattis placerat. Ut rhoncus imperdiet  nibh sit amet sagittis. Aliquam erat volutpat. In pellentesque sagittis  dictum. Aliquam erat volutpat. Donec ac aliquam neque. Sed tellus diam,  consequat nec volutpat et, cursus ac nisi. Mauris in risus in diam  <a href="#">consequat suscipit</a> non ac enim.</p>
+		    </div>
+                
+                <div class="cleaner"></div>
+            </div>
+            
+            
+
+        </div>
+        
+         <div class="services_section">
+        
+            <h4><a href="#">Q2] Signup to this application is Free ?</a></h4>
+            
+	      <div class="services_content">
+
+                
+                <div class="left_full">
+                
+                	<p>Suspendisse pulvinar nulla sed sapien egestas suscipit. Lorem ipsum  dolor sit amet, consectetur adipiscing elit. Pellentesque habitant  morbi tristique senectus et netus et <a href="#">malesuada</a> fames ac turpis egestas.  Aenean in erat odio, in tempor quam.</p>
+            </div>
+                
+                <div class="cleaner"></div>
+            </div>
+        </div>
+        
+        <div class="services_section">
+        
+            <h4><a href="#">Q3] How can i search for a Driver ?</a></h4>
+
+        <div class="services_content">
+                
+                <div class="left_full">
+                
+                	<p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per  inceptos himenaeos. Fusce consequat, erat vel <a href="#">vulputate malesuada</a>,  ipsum ligula porttitor purus, eget porta odio diam eget tellus. Nulla  tincidunt auctor justo non interdum. Cras sed diam nisi. Integer quis  purus augue.</p>
+            </div>
+                
+                <div class="cleaner"></div>
+            </div>
+        </div>
+
+        <div class="services_section">
+        
+            <h4><a href="#">Q4] How can i search for a Passenger ?</a></h4>
+
+        <div class="services_content">
+                
+                <div class="left_full">
+                
+                	<p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per  inceptos himenaeos. Fusce consequat, erat vel <a href="#">vulputate malesuada</a>,  ipsum ligula porttitor purus, eget porta odio diam eget tellus. Nulla  tincidunt auctor justo non interdum. Cras sed diam nisi. Integer quis  purus augue.</p>
+            </div>
+                
+                <div class="cleaner"></div>
+            </div>
+        </div>
+
+        <div class="services_section">
+        
+            <h4><a href="#">Q4] Where can i see my Join Requests ?</a></h4>
+
+        <div class="services_content">
+                
+                <div class="left_full ">
+                
+                	<p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per  inceptos himenaeos. Fusce consequat, erat vel <a href="#">vulputate malesuada</a>,  ipsum ligula porttitor purus, eget porta odio diam eget tellus. Nulla  tincidunt auctor justo non interdum. Cras sed diam nisi. Integer quis  purus augue.</p>
+            </div>
+                
+                <div class="cleaner"></div>
+            </div>
+        </div>
+        
+    </div> <!-- end of templatemo_content -->
+        
+    <?php //$this->load->view('sidebar'); ?>
+
+	<div class="cleaner"></div>
+</div>
+</div>
+</div>
